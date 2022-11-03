683dcfdf7557816ed3d185103b7744b32bab1098
Matt <mkiska1@gmail.com>
2022-11-03 11:10:39 -0400

    created react app

---
+++ nasa_api/src/App.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react'
+import axios from 'axios'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
